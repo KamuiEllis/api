9cab540468ba34c452bb880729f2a9c26261855d
Kamui <nathan@larvationweb.com>
2022-07-13 02:10:40 -0500

    authorization added

--- index.js
+++ index.js
@@ -8,36 +8,20 @@ const rootRouter = require('./routes/index')
 const passport = require('passport')
 const strategy = require('./strategies/local')
 var cors = require('cors')
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
 
 
 app.use(cors());
 
-
-
 const store = new session.MemoryStore();
 
-app.use(session({
-    secret: 'sup',
-    cookie: {maxAge: 30000},
-    saveUninitialized: false,
-    store
-}))
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
-app.use(passport.initialize())
-app.use(passport.session())
 
-// app.use((req,res,next) => {
-//     const authToken = req.cookies['AuthToken']
-
-//     req.user = authTokens[authToken]
-
-//     next()
-// })
-
-
-app.use('/', rootRouter)
+app.use('/', authenticate, rootRouter)
 
 mongoose.connect('mongodb://localhost:27017/testdb')
 const db = mongoose.connection;
@@ -54,14 +38,37 @@ db.once('open', () => {
 
 
 
-app.get('/', (req,res) => {
-    res.send({msg:'first route call'})
+// app.get('/', (req,res) => {
+//     res.send({msg:'first route call'})
+// })
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const user = {name: username } 
+
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({accessToken: token});
+
 })
 
-app.post('/', (req,res) => {
-    console.log(req.body)
-    res.send({data:req.body})
-})
+function authenticate(req,res,next) {
+    const auth = req.headers['authorization'];
+    const token = auth && auth.split(' ')[1];
+    if(token == null) return res.sendStatus(404);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403);
+        req.user = user;
+        console.log(user);
+        next();
+
+    })
+}
+
+// app.post('/', (req,res) => {
+//     console.log(req.body)
+//     res.send({data:req.body})
+// })
 
 app.listen(4000, () => {
     console.log('Listening on port 4000')
